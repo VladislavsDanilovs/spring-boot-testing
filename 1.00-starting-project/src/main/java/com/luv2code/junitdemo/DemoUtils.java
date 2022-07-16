dddc2660d6d793819c93ea13193e21291b877936
Vlad Danilov <vladislavs.danilovs@inbox.lv>
2022-07-16 10:28:20 +0300

    add JUnit tests (Defining names, Same/NotSame, True/False, Arrays, Iterable and Lines)

--- 1.00-starting-project/src/main/java/com/luv2code/junitdemo/DemoUtils.java
+++ 1.00-starting-project/src/main/java/com/luv2code/junitdemo/DemoUtils.java
@@ -4,10 +4,10 @@ import java.util.List;
 
 public class DemoUtils {
 
-    private String academy = "Luv2Code Academy";
+    private String academy = "Academy";
     private String academyDuplicate = academy;
     private String[] firstThreeLettersOfAlphabet = {"A", "B", "C"};
-    private List<String> academyInList = List.of("luv", "2", "code");
+    private List<String> academyInList = List.of("love", "2", "code");
 
     public List<String> getAcademyInList() {
         return academyInList;
--- 1.00-starting-project/src/test/java/com/luv2code/junitdemo/DemoUtilsTest.java
+++ 1.00-starting-project/src/test/java/com/luv2code/junitdemo/DemoUtilsTest.java
@@ -2,51 +2,35 @@ package com.luv2code.junitdemo;
 
 import org.junit.jupiter.api.*;
 
-import java.sql.SQLOutput;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+//@DisplayNameGeneration(DisplayNameGenerator.Simple.class)
 class DemoUtilsTest {
+
+    DemoUtils demoUtils;
 
     @BeforeAll
     static void setupBeforeEachClass(){
-        System.out.println("@BeforeAll executes only once before all test methods");
     }
-
-    @AfterAll
-    static void setupAfterEachClass(){
-        System.out.println("@AfterAll executes only once after all test methods");
-    }
-    DemoUtils demoUtils;
-
 
     @BeforeEach
     void setupBeforeEach() {
         demoUtils = new DemoUtils();
-        System.out.println("@BeforeEach execute before the execution of each test method");
-    }
-
-    // Normaly after each use to clean up
-    @AfterEach
-    void tearDownAfterEach() {
-        System.out.println("Running @After Each");
-        System.out.println();
     }
 
     @Test
+    @DisplayName("Equals and Not Equals")
     void testEqualsAndNotEquals() {
-
-        System.out.println("Running test: testEqualsAndNotEquals");
 
         assertEquals(6, demoUtils.add(2,4), "2+4 must be 6");
         assertNotEquals(6, demoUtils.add(1,9), "1+9 must not be 6");
-
     }
 
     @Test
+    @DisplayName("Null and Not null")
     void testNullAndNotNull() {
-
-        System.out.println("Running test: testNullAndNotNull");
 
         String str1 = null;
         String str2 = "test";
@@ -54,4 +38,67 @@ class DemoUtilsTest {
         assertNull(demoUtils.checkNull(str1), "Object should be null");
         assertNotNull(demoUtils.checkNull(str2), "Object should not be null");
     }
+
+    @DisplayName("Same and Not Same")
+    @Test
+    void testSameAndNotSame() {
+
+        String str = "random text";
+
+        assertSame(demoUtils.getAcademy(), demoUtils.getAcademyDuplicate(), "Object should refer to the same object");
+        assertNotSame(str, demoUtils.getAcademy(), "Object should not refer to the same object");
+    }
+
+    @DisplayName("True and False")
+    @Test
+    void testTrueFalse(){
+        int gradeOne = 10;
+        int gradeTwo = 5;
+
+        assertTrue(demoUtils.isGreater(gradeOne, gradeTwo), "This should return true");
+        assertFalse(demoUtils.isGreater(gradeTwo, gradeOne), "This should return false");
+    }
+
+    @DisplayName("Array Equals")
+    @Test
+    void testArrayEquals() {
+        String[] stringArray = {"A", "B", "C"};
+
+        assertArrayEquals(stringArray, demoUtils.getFirstThreeLettersOfAlphabet(), "Arrays should be the same");
+
+    }
+
+    @DisplayName("Iterable equals")
+    @Test
+    void testIterableEquals() {
+        List<String> list1 = List.of("love", "2", "code");
+
+        assertIterableEquals(list1, demoUtils.getAcademyInList(), "Expected list should be same as actual list");
+    }
+
+    @DisplayName("Lines match")
+    @Test
+    void testLinesMatch(){
+        List<String> list1 = List.of("love", "2", "code");
+
+        assertLinesMatch(list1, demoUtils.getAcademyInList(), "Lines should match");
+    }
+
+//    @BeforeEach
+//    void setupBeforeEach() {
+//        demoUtils = new DemoUtils();
+//        System.out.println("@BeforeEach execute before the execution of each test method");
+//    }
+//
+//    // Normaly after each use to clean up
+//    @AfterEach
+//    void tearDownAfterEach() {
+//        System.out.println("Running @After Each");
+//        System.out.println();
+//    }
+//
+//    @AfterAll
+//    static void setupAfterEachClass(){
+//        System.out.println("@AfterAll executes only once after all test methods");
+//    }
 }
